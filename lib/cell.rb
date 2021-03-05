ad8230755bbc72f5a39988069625386e3e068e5e
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 02:10:52 -0700

    Create activate method which passes test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,19 @@
 class Cell
-  
+
   def initialize
     @active = false
+    @target = false
   end
 
   def is_active?
     @active
+  end
+
+  def is_target?
+    @target
+  end
+
+  def activate
+    @active = true
   end
 end
