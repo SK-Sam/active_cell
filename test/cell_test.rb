ced4013627bcf6de259eab7685ab85a91332ac7f
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 01:41:04 -0700

    Change method active to become active? to keep attr private

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -9,16 +9,19 @@ class CellTest < MiniTest::Test
 
   def test_it_can_instantiate_and_have_default_values
     assert_instance_of Cell, @cell
-    assert false, @cell.active
     assert false, @cell.target
   end
 
+  def test_it_can_check_if_its_active
+    assert false, @cell.active?
+  end
+
   def test_it_can_be_activated
-    assert false, @cell.active
+    assert false, @cell.active?
 
     @cell.activate
     
-    assert true, @cell.active
+    assert true, @cell.active?
   end
 
   def test_it_can_be_chosen_as_cell_that_will_check_values_near_it
