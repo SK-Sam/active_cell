10530850d20f731bbe1ebad4d4c4c4b6fb4bb611
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 02:19:50 -0700

    Add test for targeting cell

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -18,12 +18,18 @@ class CellTest < MiniTest::Test
   def test_it_can_check_if_its_target_and_has_default_value
     assert_equal false, @cell.is_target?
   end
-  
+
   def test_it_can_be_activated
     assert_equal false, @cell.is_active?
 
     @cell.activate
     
     assert_equal true, @cell.is_active?
+  end
+
+  def test_it_can_be_targeted
+    @cell.target
+    
+    assert_equal true, @cell.is_target?
   end
 end
