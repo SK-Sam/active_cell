711546b356cd397206f7f287351d8b29e61ef29b
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 07:35:29 -0700

    Add test for Board helper method all_cells_evaluated

--- test/board_test.rb
+++ test/board_test.rb
@@ -16,6 +16,25 @@ class BoardTest < MiniTest::Test
     assert_equal true, @board.cells.flatten.all? { |cell| cell.class == Cell }
   end
 
+  def test_it_can_evaluate_all_cells_as_2d_array
+    default_board_evaluated = [
+      ['.', '.', '.'],
+      ['.', '.', '.'],
+      ['.', '.', '.']
+    ]
+
+    assert_equal default_board_evaluated, @board.all_cells_evaluated
+
+    board_with_active_cell = [
+      ['.', '!', '.'],
+      ['.', '.', '.'],
+      ['.', '.', '.']
+    ]
+    @board.cells[0][1].activate
+
+    assert_equal board_with_active_cell, @board.all_cells_evaluated
+  end
+
   def test_it_can_render_default_board
     board_layout =
     "  1 2 3\n" + 
