d59884d3f9e384e00b579d009f74908cb8e3f4a4
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 01:49:58 -0700

    Change assertion to assert_equal to specify instead of truthy

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -9,18 +9,18 @@ class CellTest < MiniTest::Test
 
   def test_it_can_instantiate_and_have_default_values
     assert_instance_of Cell, @cell
-    assert false, @cell.target
+    assert_equal false, @cell.target
   end
 
   def test_it_can_check_if_its_active
-    assert false, @cell.is_active?
+    assert_equal false, @cell.is_active?
   end
 
   def test_it_can_be_activated
-    assert false, @cell.is_active?
+    assert_equal false, @cell.is_active?
 
     @cell.activate
     
-    assert true, @cell.is_active?
+    assert_equal true, @cell.is_active?
   end
 end
