2d5076899626cd62035ef3e55ba625699be0af50
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 13:30:10 -0700

    Create helper method tests for horizontal and vertical checks

--- test/simulation_test.rb
+++ test/simulation_test.rb
@@ -37,18 +37,62 @@ class SimulationTest < MiniTest::Test
     assert_equal false, @simulation.board.cells.flatten.all? { |cell| cell.is_active? == false }
   end
 
+  def test_it_can_check_up
+    @simulation.check_up
+
+    assert_equal 0, @simulation.active_cells_near_target_count
+
+    @simulation.board.cells[0][1].activate
+    @simulation.check_up
+
+    assert_equal 1, @simulation.active_cells_near_target_count
+  end
+
+  def test_it_can_check_down
+    @simulation.check_down
+
+    assert_equal 0, @simulation.active_cells_near_target_count
+
+    @simulation.board.cells[2][1].activate
+    @simulation.check_down
+
+    assert_equal 1, @simulation.active_cells_near_target_count
+  end
+
+  def test_it_can_check_left
+    @simulation.check_left
+
+    assert_equal 0, @simulation.active_cells_near_target_count
+
+    @simulation.board.cells[1][0].activate
+    @simulation.check_left
+
+    assert_equal 1, @simulation.active_cells_near_target_count
+  end
+
+  def test_it_can_check_right
+    @simulation.check_right
+
+    assert_equal 0, @simulation.active_cells_near_target_count
+
+    @simulation.board.cells[1][2].activate
+    @simulation.check_right
+
+    assert_equal 1, @simulation.active_cells_near_target_count
+  end
+
   def test_it_can_check_cell_horizontal_neighbors
     @simulation.stubs(:rand).returns(1)
     @simulation.set_active_cells
     @simulation.target_horizontal_check
     
-    assert_equal 2, @simulation.active_cell_near_target_count
+    assert_equal 2, @simulation.active_cells_near_target_count
 
     simulation_2 = Simulation.new
     simulation_2.board.cells[1][0].activate
     simulation_2.target_horizontal_check
 
-    assert_equal 1, simulation_2.active_cell_near_target_count
+    assert_equal 1, simulation_2.active_cells_near_target_count
   end
 
   def test_it_can_check_cell_vertical_neighbors
@@ -56,13 +100,13 @@ class SimulationTest < MiniTest::Test
     @simulation.set_active_cells
     @simulation.target_vertical_check
     
-    assert_equal 2, @simulation.active_cell_near_target_count
+    assert_equal 2, @simulation.active_cells_near_target_count
 
     simulation_2 = Simulation.new
     simulation_2.board.cells[0][1].activate
     simulation_2.target_vertical_check
 
-    assert_equal 1, simulation_2.active_cell_near_target_count
+    assert_equal 1, simulation_2.active_cells_near_target_count
   end
 
   def test_it_can_check_cell_diagonal_neighbors
@@ -70,27 +114,27 @@ class SimulationTest < MiniTest::Test
     @simulation.set_active_cells
     @simulation.target_diagonal_check
     
-    assert_equal 4, @simulation.active_cell_near_target_count
+    assert_equal 4, @simulation.active_cells_near_target_count
 
     simulation_2 = Simulation.new
     simulation_2.board.cells[0][2].activate
     simulation_2.target_diagonal_check
 
-    assert_equal 1, simulation_2.active_cell_near_target_count
+    assert_equal 1, simulation_2.active_cells_near_target_count
 
     simulation_2.board.cells[2][2].activate
     simulation_2.target_diagonal_check
 
-    assert_equal 2, simulation_2.active_cell_near_target_count
+    assert_equal 2, simulation_2.active_cells_near_target_count
 
     simulation_2.board.cells[0][0].activate
     simulation_2.target_diagonal_check
 
-    assert_equal 3, simulation_2.active_cell_near_target_count
+    assert_equal 3, simulation_2.active_cells_near_target_count
 
     simulation_2.board.cells[2][0].activate
     simulation_2.target_diagonal_check
 
-    assert_equal 3, simulation_2.active_cell_near_target_count
+    assert_equal 3, simulation_2.active_cells_near_target_count
   end
 end
