16eb7a289a9626b57cde4bb28a8260e2d96f8adf
Sam Yeo <yeosamuel95@gmail.com>
2021-03-05 15:52:52 -0700

    Create helper methods for diagonal, horizontal, vertical checks which pass tests

--- lib/simulation.rb
+++ lib/simulation.rb
@@ -1,7 +1,7 @@
 class Simulation
   attr_reader :board,
               :target_cell,
-              :active_cell_near_target_count
+              :active_cells_near_target_count
 
   def initialize(target_cell = [1, 1])
     @board = Board.new
@@ -9,7 +9,7 @@ class Simulation
       row: target_cell.first,
       column: target_cell.last
     }
-    @active_cell_near_target_count = 0
+    @active_cells_near_target_count = 0
   end
 
   def target_center_cell
@@ -26,35 +26,79 @@ class Simulation
 
   def target_horizontal_check
     @active_cell_near_target_count = 0
-    row = @target_cell[:row]
-    column = @target_cell[:column]
-    out_of_bounds_left = 0
-    out_of_bounds_right = @board.cells[row].count - 1 # should be 2 in a 3x3 2d array
-    
-    if column - 1 >= out_of_bounds_left && @board.cells[row][column - 1].is_active?
-      @active_cell_near_target_count += 1
-    end
-    if column + 1 <= out_of_bounds_right && @board.cells[row][column + 1].is_active?
-      @active_cell_near_target_count += 1
-    end
+    check_left
+    check_right
   end
 
   def target_vertical_check
     @active_cell_near_target_count = 0
-    row = @target_cell[:row]
-    column = @target_cell[:column]
-    out_of_bounds_top = 0
-    out_of_bounds_bottom = @board.cells.count - 1 # should be 2 in a 3x3 2d array
-    
-    if row - 1 >= out_of_bounds_top && @board.cells[row - 1][column].is_active?
-      @active_cell_near_target_count += 1
-    end
-    if row + 1 <= out_of_bounds_bottom && @board.cells[row + 1][column].is_active?
-      @active_cell_near_target_count += 1
-    end
+    check_up
+    check_down
   end
 
   def target_diagonal_check
+    check_diagonal_top
+    check_diagonal_bottom
+  end
 
+  def check_up
+    row = @target_cell[:row]
+    column = @target_cell[:column]
+    out_of_bounds_top = 0
+
+    if row - 1 >= out_of_bounds_top && @board.cells[row - 1][column].is_active?
+      @active_cells_near_target_count += 1
+    end
+  end
+
+  def check_down
+    row = @target_cell[:row]
+    column = @target_cell[:column]
+    out_of_bounds_bottom = @board.cells.count - 1
+
+    if row + 1 <= out_of_bounds_bottom && @board.cells[row + 1][column].is_active?
+      @active_cells_near_target_count += 1
+    end
+  end
+
+  def check_left
+    row = @target_cell[:row]
+    column = @target_cell[:column]
+    out_of_bounds_left = 0
+
+    if column - 1 >= out_of_bounds_left && @board.cells[row][column - 1].is_active?
+      @active_cells_near_target_count += 1
+    end
+  end
+
+  def check_right
+    row = @target_cell[:row]
+    column = @target_cell[:column]
+    out_of_bounds_right = @board.cells[row].count - 1
+
+    if column + 1 <= out_of_bounds_right && @board.cells[row][column + 1].is_active?
+      @active_cells_near_target_count += 1
+    end
+  end
+
+  # Using guard clause to not even check the diagonal tops if out of bounds
+  def check_diagonal_top
+    row = @target_cell[:row]
+    column = @target_cell[:column]
+    out_of_bounds_top = 0
+
+    return if row - 1 < out_of_bounds_top
+    @active_cells_near_target_count += 1 if @board.cells[row - 1][column - 1] != nil && @board.cells[row - 1][column - 1].is_active?
+    @active_cells_near_target_count += 1 if @board.cells[row - 1][column + 1] != nil && @board.cells[row - 1][column + 1].is_active?
+  end
+
+  def check_diagonal_bottom
+    row = @target_cell[:row]
+    column = @target_cell[:column]
+    out_of_bounds_bottom = @board.cells.count - 1
+
+    return if row + 1 > out_of_bounds_bottom
+    @active_cells_near_target_count += 1 if @board.cells[row + 1][column - 1] != nil && @board.cells[row + 1][column - 1].is_active?
+    @active_cells_near_target_count += 1 if @board.cells[row + 1][column + 1] != nil && @board.cells[row + 1][column + 1].is_active?
   end
 end
